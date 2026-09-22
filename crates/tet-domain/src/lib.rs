48a029e71047072daf960cffdc33a6b0bcbbb1db
mmb L <7960706+mammothb@users.noreply.github.com>
2026-09-22 21:23:34 +0800

    feat: board (#2)
    * feat: board domain model
    
    * test: board tests

--- crates/tet-domain/src/lib.rs
+++ crates/tet-domain/src/lib.rs
@@ -1,8 +1,10 @@
+pub mod board;
 pub mod mino;
 pub mod orientation;
 pub mod ruleset;
 pub mod spatial;
 
+pub use board::Board;
 pub use mino::MinoType;
 pub use orientation::Orientation;
 pub use ruleset::Ruleset;
